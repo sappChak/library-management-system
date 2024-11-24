f14718f7f7c8d62b40027fb8ce7ee496c82ac921
Andrii Konotop <konotop401@gmail.com>
2024-11-24 18:32:15 +0100

    feat: add repositories for User and RefreshToken entities

---
+++ src/main/java/org/example/bookstore/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package org.example.bookstore.repository;
+
+import java.util.Optional;
+
+import org.example.bookstore.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByUsername(String username);
+
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserById(Long id);
+
+}
