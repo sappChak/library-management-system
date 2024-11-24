c8d6b065118718cd15b9e47182f484b6c6f38eff
Andrii Konotop <konotop401@gmail.com>
2024-11-24 21:06:24 +0100

    fix(annotations): replace EmailValidator with EmailValidation in ValidEmail annotation

--- src/main/java/org/example/bookstore/annotations/ValidEmail.java
+++ src/main/java/org/example/bookstore/annotations/ValidEmail.java
@@ -6,14 +6,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.hibernate.validator.internal.constraintvalidators.bv.EmailValidator;
+import org.example.bookstore.validations.EmailValidation;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
 @Target({ ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = EmailValidator.class)
+@Constraint(validatedBy = EmailValidation.class)
 @Documented
 public @interface ValidEmail {
 
