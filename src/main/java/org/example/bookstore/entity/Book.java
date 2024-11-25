39caef6ed8a7659d378e2d018285210b385a3513
Andrii Konotop <konotop401@gmail.com>
2024-11-25 11:34:20 +0100

    refactor: update entity annotations and field names

--- src/main/java/org/example/bookstore/entity/Book.java
+++ src/main/java/org/example/bookstore/entity/Book.java
@@ -1,20 +1,32 @@
 package org.example.bookstore.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Entity
-@Table(name = "books")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long bookId;
+    private Long id;
 
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String author;
+
+    @Column(nullable = false, unique = true)
     private String isbn;
-    private Integer availableCopies;
+
+    @Column(nullable = false)
+    private int availableCopies;
 }
--- src/main/java/org/example/bookstore/entity/Transaction.java
+++ src/main/java/org/example/bookstore/entity/Transaction.java
@@ -2,16 +2,21 @@ package org.example.bookstore.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Entity
-@Table(name = "transactions")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,6 +30,9 @@ public class Transaction {
     @JoinColumn(name = "book_id", nullable = false)
     private Book book;
 
+    @Column(nullable = false)
     private String action;
+
+    @Column(nullable = false)
     private LocalDateTime date;
 }
