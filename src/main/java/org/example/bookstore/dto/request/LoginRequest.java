228c539b9f0d668620fcb60724f2114a00c4450b
Andrii Konotop <konotop401@gmail.com>
2024-11-25 08:01:06 +0100

    docs: add Swagger annotations to LoginRequest

--- src/main/java/org/example/bookstore/dto/request/LoginRequest.java
+++ src/main/java/org/example/bookstore/dto/request/LoginRequest.java
@@ -1,14 +1,18 @@
 package org.example.bookstore.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
+@Schema(description = "Request object for user login")
 public class LoginRequest {
 
         @NotEmpty(message = "Username cannot be empty")
+        @Schema(description = "Username of the user", example = "john_doe")
         private String username;
 
         @NotEmpty(message = "Password cannot be empty")
+        @Schema(description = "Password of the user", example = "P@ssw0rd")
         private String password;
 }
