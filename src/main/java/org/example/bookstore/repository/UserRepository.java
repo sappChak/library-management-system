05221d6f846b9e878298dea88c3a5efad09b9dc1
Andrii Konotop <konotop401@gmail.com>
2024-11-25 07:54:13 +0100

    feat: add methods to check existence by email and username

--- src/main/java/org/example/bookstore/repository/UserRepository.java
+++ src/main/java/org/example/bookstore/repository/UserRepository.java
@@ -15,4 +15,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserById(Long id);
 
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
 }
