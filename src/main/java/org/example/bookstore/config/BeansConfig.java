269e25606833c0df0a925a53fbacbae25043320e
Andrii Konotop <konotop401@gmail.com>
2024-11-25 08:00:41 +0100

    refactor: add logging

--- src/main/java/org/example/bookstore/config/BeansConfig.java
+++ src/main/java/org/example/bookstore/config/BeansConfig.java
@@ -1,6 +1,7 @@
 package org.example.bookstore.config;
 
-import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,22 +11,29 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @RequiredArgsConstructor
 public class BeansConfig {
 
+        private static final Logger logger = LoggerFactory.getLogger(BeansConfig.class);
+
         @Bean
         AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+                logger.info("Configuring AuthenticationManager");
                 return authConfig.getAuthenticationManager();
         }
 
         @Bean
         BCryptPasswordEncoder passwordEncoder() {
+                logger.info("Configuring BCryptPasswordEncoder");
                 return new BCryptPasswordEncoder();
         }
 
         @Bean
         CorsConfigurationSource corsConfigurationSource() {
+                logger.info("Configuring CORS settings");
                 CorsConfiguration config = new CorsConfiguration();
                 config.setAllowCredentials(true);
                 config.addAllowedOriginPattern("*");
@@ -35,6 +43,7 @@ public class BeansConfig {
                 UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
                 source.registerCorsConfiguration("/**", config);
 
+                logger.info("CORS configured to allow all origins, headers, and methods.");
                 return source;
         }
 }
