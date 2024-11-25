984d7cc5a7bc1b8a0fc15004aa6bea3356199118
Andrii Konotop <konotop401@gmail.com>
2024-11-25 14:38:31 +0100

    feat(config): add JWT bearer authentication to OpenAPI configuration

--- src/main/java/org/example/bookstore/config/OpenAPIConfig.java
+++ src/main/java/org/example/bookstore/config/OpenAPIConfig.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
@@ -20,14 +23,28 @@ public class OpenAPIConfig {
       server.setDescription("Development");
 
       Contact myContact = new Contact();
-      myContact.setName("Jane Doe");
-      myContact.setEmail("your.email@gmail.com");
+      myContact.setName("Andrii Konotop");
+      myContact.setEmail("konotop401@gmail.com");
 
       Info information = new Info()
-            .title("Employee Management System API")
+            .title("Library API")
             .version("1.0")
-            .description("This API exposes endpoints to manage employees.")
+            .description("This API exposes endpoints to manage library.")
             .contact(myContact);
-      return new OpenAPI().info(information).servers(List.of(server));
+
+      SecurityScheme securityScheme = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .in(SecurityScheme.In.HEADER)
+            .name("Authorization");
+
+      SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+      return new OpenAPI()
+            .info(information)
+            .servers(List.of(server))
+            .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+            .addSecurityItem(securityRequirement);
    }
 }
