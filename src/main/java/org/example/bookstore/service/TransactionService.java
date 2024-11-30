2cdec90129452c5dcce377e1edeac516ac157341
Andrii Konotop <konotop401@gmail.com>
2024-11-30 20:57:29 +0100

    feat(transactions): get borrowed books with `isActive` flag only

--- src/main/java/org/example/bookstore/service/TransactionService.java
+++ src/main/java/org/example/bookstore/service/TransactionService.java
@@ -8,6 +8,8 @@ import org.example.bookstore.entity.Transaction;
 import org.example.bookstore.entity.User;
 import org.example.bookstore.entity.enums.ActionType;
 import org.example.bookstore.repository.TransactionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -19,6 +21,8 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class TransactionService {
 
+    private static final Logger logger = LoggerFactory.getLogger(TransactionService.class);
+
     private final TransactionRepository transactionRepository;
     private final UserService userService;
 
@@ -29,10 +33,13 @@ public class TransactionService {
                 userId, book.getId(), ActionType.BORROW, true);
 
         if (action == ActionType.BORROW && hasActiveBorrow) {
+            logger.error("User {} already has an active borrow transaction for book {}", user.getUsername(),
+                    book.getTitle());
             throw new IllegalArgumentException("You already have an active borrow transaction for this book.");
         }
 
         if (action == ActionType.RETURN && !hasActiveBorrow) {
+            logger.error("User {} hasn't borrowed book {}", user.getUsername(), book.getTitle());
             throw new IllegalArgumentException("You cannot return a book you haven't borrowed.");
         }
 
@@ -41,6 +48,9 @@ public class TransactionService {
         transaction.setBook(book);
         transaction.setAction(action);
         transaction.setDate(LocalDateTime.now());
+
+        logger.info("Recording transaction for user {} for book {} with action {}", user.getUsername(), book.getTitle(),
+                action);
 
         if (action == ActionType.RETURN) {
             Transaction activeBorrow = transactionRepository.findFirstByUserIdAndBookIdAndActionAndIsActive(
@@ -61,6 +71,10 @@ public class TransactionService {
 
     public List<Transaction> getTransactionsByUserId(Long userId) {
         return transactionRepository.findAllByUserId(userId);
+    }
+
+    public List<Transaction> getTransactionsWithoutActiveBorrow(Long userId) {
+        return transactionRepository.findAllByUserIdAndIsActive(userId, true);
     }
 
     public List<Transaction> getAllTransactions() {
