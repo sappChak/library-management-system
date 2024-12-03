214ffd25eda82bcc3a145bffa2214c383599fd84
Andrii Konotop <konotop401@gmail.com>
2024-12-03 20:26:23 +0100

    chore(ui): add eslint and prettier configuration

---
+++ bookstore-ui/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+  ],
+  rules: {
+    'vue/no-multiple-template-root': 'off',
+  },
+}
