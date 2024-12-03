a7c071c9b669f5a583bcc1e350360db174e76e77
Andrii Konotop <konotop401@gmail.com>
2024-12-03 16:00:22 +0100

    feat(router): add admin role restriction

--- bookstore-ui/src/router/auth.guard.ts
+++ bookstore-ui/src/router/auth.guard.ts
@@ -1,13 +1,24 @@
 import { useAuthStore } from "@/store/auth.store";
+import { NavigationGuardNext, RouteLocationNormalized } from "vue-router";
 
-export const authGuard = (to: any, _from: any, next: any) => {
+export const authGuard = (
+  to: RouteLocationNormalized,
+  _from: RouteLocationNormalized,
+  next: NavigationGuardNext,
+) => {
   const authStore = useAuthStore();
 
   const isAuthenticated = authStore.isAuthenticated;
 
   if (to.meta.requiresAuth && !isAuthenticated) {
     next("/login");
-  } else {
-    next();
+    return;
   }
+
+  if (to.meta.requiresAdmin && !authStore.isAdmin) {
+    next("/");
+    return;
+  }
+
+  next();
 };
