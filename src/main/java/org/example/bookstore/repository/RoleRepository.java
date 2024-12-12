0219cfc110a3f9bac8c6f354a83a320570781e1d
Andrii Konotop <konotop401@gmail.com>
2024-12-12 09:35:23 +0100

    refactor: update RoleRepository to use ERole enum

--- src/main/java/org/example/bookstore/repository/RoleRepository.java
+++ src/main/java/org/example/bookstore/repository/RoleRepository.java
@@ -5,10 +5,11 @@ import java.util.Optional;
 import java.util.Set;
 
 import org.example.bookstore.entity.Role;
+import org.example.bookstore.entity.enums.ERole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String roleName);
+    Optional<Role> findByName(ERole roleName);
 
-    List<Role> findAllByNameIn(Set<String> roleNames);
+    List<Role> findAllByNameIn(Set<ERole> roleNames);
 }
